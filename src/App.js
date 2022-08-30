137b45baeb65190971c39f1e820e7f5c241cc7b6
impossibleshadowstorm <impossibleshadowstorm@gmail.com>
2022-08-30 16:58:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Box } from "@mui/material";
+import Slider from "./components/slider";
 
 function App() {
   return (
     <Box sx={{ height: "100vh", width: "100vw", backgroundColor: "red" }}>
       <Box
-        sx={{ height: "80vh", width: "40vw", backgroundColor: "yellow" }}
-      ></Box>
+        sx={{ height: "80vh", width: "30vw", backgroundColor: "yellow", padding:"20px" }}
+      >
+        <Slider />
+      </Box>
     </Box>
   );
 }
---
+++ src/components/slider.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Box} from "@mui/material";
+
+const Slider = () =>{
+    return (
+        <Box sx={{height: "40px", width: "100%", backgroundColor: "blue", display: "flex"}}></Box>
+    );
+}
+
+export default Slider;
