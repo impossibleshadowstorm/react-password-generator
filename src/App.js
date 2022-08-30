a827506ab9b8ac1e4dd5f3031d3be6315d4d67ce
kajalrituraj <kajalrituraj2002@gmail.com>
2022-08-30 16:59:22 +0530

    hdl

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import { Box } from "@mui/material";
 
 function App() {
   return (
-    <Box sx={{ height: "100vh", width: "100vw", backgroundColor: "red" }}>
+    <Box sx={{ height: "100vh", width: "100vw", backgroundColor: "red", display:"flex",
+    justifyContent:"center",alignItems:"center" }}>
       <Box
-        sx={{ height: "80vh", width: "40vw", backgroundColor: "yellow" }}
-      ></Box>
+        sx={{ height: "80vh", width: "30vw", backgroundColor: "yellow" ,borderRadius:"5px",border:"1px solid black",
+      
+     }}
+      >
+        <Box sx={{ height: "50vh", width: "30vw", backgroundColor: "cyan",borderRadius:"5px 5px 0px 0px",
+      
+    }}></Box>
+    <Box sx={{ height: "30vh", width: "30vw", backgroundColor: "pink",borderRadius:" 0px 0px 5px 5px",
+      
+    }}></Box>
+      </Box>
     </Box>
   );
 }
